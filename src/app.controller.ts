6857828794f29f5982cf9694906f60192b3f6896
Alberto Maturano <alberto@maturano.mx>
2023-06-15 19:47:18 -0600

    mininal reproduction code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+const OPTIONS = ['one', 'two', 'three'] as const;
+
+class ParamsDto {
+  // input text will have `something` as value
+  @ApiPropertyOptional({
+    default: 'default is ignored',
+    description: 'some string',
+    example: 'something',
+  })
+  q?: string;
+
+  // No option selected but the request will include `?options=one`
+  @ApiPropertyOptional({
+    description: 'some options',
+    enum: OPTIONS,
+    example: OPTIONS[0],
+    isArray: true,
+  })
+  options?: (typeof OPTIONS)[number];
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query() paramsDto: ParamsDto): ParamsDto {
+    return paramsDto;
   }
 }
